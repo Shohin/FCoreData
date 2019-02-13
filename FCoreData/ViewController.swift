e6c359afc5873edcc69af0bca8705fc7bb83a0da
Shohin <tagayev.shohin@gmail.com>
2019-02-13 09:14:34 +0500

    Initial Commit

---
+++ FCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FCoreData
+//
+//  Created by Shohin Tagaev on 2/13/19.
+//  Copyright © 2019 Shohin Tagaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
